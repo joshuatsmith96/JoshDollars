919cbe9c4373f84ffb24a3d4c9a88ea8e877d3d8
Joshua Smith <61332947+joshuatsmith96@users.noreply.github.com>
2023-12-19 12:06:47 -0600

    Added Nav, Footer and Home

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,17 @@ import Home from './pages/Home/Home'
 import {Routes, Route} from 'react-router-dom'
 
 //Global Components
-// import Nav from './components/Nav'
-// import Footer from './components/Footer'
+import Nav from './components/Nav'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <div className="App">
+      <Nav />
       <Routes>
         <Route path="/JoshDollars/" Component={Home}/>
       </Routes>
+      <Footer />
     </div>
   )
 }
