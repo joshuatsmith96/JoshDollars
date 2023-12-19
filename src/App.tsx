109cf57af690a3c57e54d9776a169df5c8d7132f
Joshua Smith <61332947+joshuatsmith96@users.noreply.github.com>
2023-12-19 11:40:45 -0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,13 @@ import './App.css'
 // import {Routes, Route} from 'react-router-dom'
 
 //Global Components
-// import Navigation from './components/Nav'
+import Navigation from './components/Nav'
 // import Footer from './components/Footer'
 
 function App() {
   return (
     <div className="App">
-      <h1>MAIN APP</h1>
+      <Navigation />
     </div>
   )
 }
