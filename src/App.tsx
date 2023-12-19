a0337501c1633413a6f308da58abc64909b9eac0
Joshua Smith <61332947+joshuatsmith96@users.noreply.github.com>
2023-12-19 12:03:49 -0600

    ghpages update

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import './App.css'
-// import Home from './pages/Home/Home'
-// import {Routes, Route} from 'react-router-dom'
+import Home from './pages/Home/Home'
+import {Routes, Route} from 'react-router-dom'
 
 //Global Components
 // import Nav from './components/Nav'
@@ -9,7 +9,9 @@ import './App.css'
 function App() {
   return (
     <div className="App">
-      <h1>Nav should be above this line</h1>
+      <Routes>
+        <Route path="/JoshDollars/" Component={Home}/>
+      </Routes>
     </div>
   )
 }
