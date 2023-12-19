65ef353d32fe6afcc366c423520794c8e226d2e2
Joshua Smith <61332947+joshuatsmith96@users.noreply.github.com>
2023-12-19 10:45:08 -0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import './App.css'
-import Home from './pages/Home/Home'
-import {Routes, Route} from 'react-router-dom'
+// import Home from './pages/Home/Home'
+// import {Routes, Route} from 'react-router-dom'
 
 //Global Components
 // import Navigation from './components/Nav'
@@ -10,9 +10,6 @@ function App() {
   return (
     <div className="App">
       <h1>MAIN APP</h1>
-      <Routes>
-        <Route path="/JoshDollars/" element={ <Home /> } />
-      </Routes>
     </div>
   )
 }
