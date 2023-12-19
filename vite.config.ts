02f0cb0637936c90acf8c3ba54ec78a02e9ee7cf
Joshua Smith <61332947+joshuatsmith96@users.noreply.github.com>
2023-12-19 12:30:13 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "/JoshDollars/"
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: '/JoshDollars/',
+  }
+
+  if (command !== 'serve') {
+    config.base = '/JoshDollars/'
+  }
+
+  return config
 })
